4f0cfcb09612de6604c4107d4011c20ad8d65610
Maël <156932965+MaelBTS@users.noreply.github.com>
2025-02-25 11:37:08 +0100

    Add files via upload

---
+++ portefolio/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var burger = document.getElementById('burger');
+    var menu = document.getElementById('menu');
+
+    burger.addEventListener('click', function() {
+        menu.classList.toggle('show');
+    });
+});
